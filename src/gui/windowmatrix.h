37a33130b8367434b219b1bdfbd2d5fa7702d745
mwthinker <mwthinker@yahoo.se>
2014-10-07 00:03:15 +0200

    Updated submodule.
    MwLibrary2 updated. And faster use of shader.

--- src/gui/windowmatrix.h
+++ src/gui/windowmatrix.h
@@ -18,6 +18,14 @@ namespace gui {
 	public:
 		WindowMatrix() : proj44_(mw::I_44), model44_(mw::I_44) {
 			shader_ = mw::Shader::getDefaultShader();
+			shader_->glUseProgram();
+			uModelIndex_ = shader_->getUniformLocation(mw::SHADER_U_MAT4_MODEL);
+			uProjIndex_ = shader_->getUniformLocation(mw::SHADER_U_MAT4_PROJ);
+			uColorIndex_ = shader_->getUniformLocation(mw::SHADER_U_VEC4_COLOR);
+			uIsTexIndex_ = shader_->getUniformLocation(mw::SHADER_U_FLOAT_TEXTURE);
+
+			aVertexIndex_ = shader_->getAttributeLocation(mw::SHADER_A_VEC4_POSITION);
+			aTexIndex_ = shader_->getAttributeLocation(mw::SHADER_A_VEC2_TEXCOORD);
 		}
 
 		mw::Matrix44 getProjection() const {
@@ -34,48 +42,48 @@ namespace gui {
 
 		void setProjection(const mw::Matrix44& proj) {
 			proj44_ = proj;
-			mw::glUniformMatrix4fv(shader_->getUniformLocation(mw::SHADER_U_MAT4_PROJ), 1, false, proj44_.data());
+			mw::glUniformMatrix4fv(uProjIndex_, 1, false, proj44_.data());
 		}
 
 		void setModel(const mw::Matrix44& model) {
 			model44_ = model;
-			mw::glUniformMatrix4fv(shader_->getUniformLocation(mw::SHADER_U_MAT4_MODEL), 1, false, model44_.data());
+			mw::glUniformMatrix4fv(uModelIndex_, 1, false, model44_.data());
 		}
 		
 		void setVertexPosition(GLint dimension, const GLvoid* data) {
-			mw::glVertexAttribPointer(shader_->getAttributeLocation(mw::SHADER_A_VEC4_POSITION), dimension, GL_FLOAT, GL_FALSE, 0, data);
-			mw::glEnableVertexAttribArray(shader_->getAttributeLocation(mw::SHADER_A_VEC4_POSITION));
+			mw::glVertexAttribPointer(aVertexIndex_, dimension, GL_FLOAT, GL_FALSE, 0, data);
+			mw::glEnableVertexAttribArray(aVertexIndex_);
 		}
 
 		void setTexturePosition(GLint dimension, const GLvoid* data) {
-			mw::glVertexAttribPointer(shader_->getAttributeLocation(mw::SHADER_A_VEC2_TEXCOORD), dimension, GL_FLOAT, GL_FALSE, 0, data);
-			mw::glEnableVertexAttribArray(shader_->getAttributeLocation(mw::SHADER_A_VEC2_TEXCOORD));
+			mw::glVertexAttribPointer(aTexIndex_, dimension, GL_FLOAT, GL_FALSE, 0, data);
+			mw::glEnableVertexAttribArray(aTexIndex_);
 		}
 
 		void setVertexPosition(GLint dimension, GLsizei stride, const GLvoid* data) {
-			mw::glVertexAttribPointer(shader_->getAttributeLocation(mw::SHADER_A_VEC4_POSITION), dimension, GL_FLOAT, GL_FALSE, stride, data);
-			mw::glEnableVertexAttribArray(shader_->getAttributeLocation(mw::SHADER_A_VEC4_POSITION));
+			mw::glVertexAttribPointer(aVertexIndex_, dimension, GL_FLOAT, GL_FALSE, stride, data);
+			mw::glEnableVertexAttribArray(aVertexIndex_);
 		}
 
 		void setTexturePosition(GLint dimension, GLsizei stride, const GLvoid* data) {
-			mw::glVertexAttribPointer(shader_->getAttributeLocation(mw::SHADER_A_VEC2_TEXCOORD), dimension, GL_FLOAT, GL_FALSE, stride, data);
-			mw::glEnableVertexAttribArray(shader_->getAttributeLocation(mw::SHADER_A_VEC2_TEXCOORD));
+			mw::glVertexAttribPointer(aTexIndex_, dimension, GL_FLOAT, GL_FALSE, stride, data);
+			mw::glEnableVertexAttribArray(aTexIndex_);
 		}
 
 		void setTexture(bool texture) {
 			if (texture) {
-				mw::glUniform1f(shader_->getUniformLocation(mw::SHADER_U_FLOAT_TEXTURE), 1);
+				mw::glUniform1f(uIsTexIndex_, 1);
 			} else {
-				mw::glUniform1f(shader_->getUniformLocation(mw::SHADER_U_FLOAT_TEXTURE), 0);
+				mw::glUniform1f(uIsTexIndex_, 0);
 			}
 		}
 
 		void setColor(const mw::Color& color) {
-			mw::glUniform4f(shader_->getUniformLocation(mw::SHADER_U_VEC4_COLOR), color.red_, color.green_, color.blue_, color.alpha_);
+			mw::glUniform4f(uColorIndex_, color.red_, color.green_, color.blue_, color.alpha_);
 		}
 
 		void setColor(float red, float green, float blue, float alpha = 1.f) {
-			mw::glUniform4f(shader_->getUniformLocation(mw::SHADER_U_VEC4_COLOR), red, green, blue, alpha);
+			mw::glUniform4f(uColorIndex_, red, green, blue, alpha);
 		}
 
 		void glDrawArrays(GLenum mode, GLint first, GLsizei count) {
@@ -88,6 +96,12 @@ namespace gui {
 		
 	private:
 		mw::Matrix44 model44_, proj44_;
+		int aVertexIndex_;
+		int aTexIndex_;
+		int uIsTexIndex_;
+		int uColorIndex_;
+		int uModelIndex_;
+		int uProjIndex_;
 		mw::ShaderPtr shader_;
 	};
 
